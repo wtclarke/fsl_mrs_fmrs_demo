85a02eab48f10aa1056aef7cfad454c19c799df0
wtclarke <william.clarke@ndcn.ox.ac.uk>
2023-02-16 12:03:22 +0000

    Updated low resolution first level fit.

--- simulated_data/low_res/reduce_res.py
+++ simulated_data/low_res/reduce_res.py
@@ -1,8 +1,15 @@
+'''Script to generate the low resolution versions of the simulated data
+for the first_level_fit_low_res_demo.ipynb example
+
+run from the base directory of this repo:
+    python reduce_res.py
+'''
+
 import numpy as np
 
 from fsl_mrs.utils import mrs_io
 from fsl_mrs.utils.preproc import nifti_mrs_proc as proc
-from fsl_mrs.utils import nifti_mrs_tools as ntools
+from fsl_mrs.core import nifti_mrs as ntools
 
 
 def block_average(data, indices):
@@ -19,8 +26,8 @@ print(blk_indices)
 
 open_fullres = mrs_io.read_FID('simulated_data/sub0_stim.nii.gz')
 block_average(open_fullres, blk_indices)\
-    .save('simulated_data/lowres_stim.nii.gz')
+    .save('simulated_data/low_res/lowres_stim.nii.gz')
 
 closed_fullres = mrs_io.read_FID('simulated_data/sub0_ctrl.nii.gz')
 block_average(closed_fullres, blk_indices)\
-    .save('simulated_data/lowres_ctrl.nii.gz')
+    .save('simulated_data/low_res/lowres_ctrl.nii.gz')
